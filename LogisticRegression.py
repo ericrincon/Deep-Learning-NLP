5a90222f1fb31dcd6690cef094b40b42abcd67bc
eric147 <eric_rincon@Erics-MacBook-Pro.local>
2014-11-03 18:35:34 -0600

    Update

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -3,53 +3,121 @@ __author__ = 'eric_rincon'
 import theano
 import numpy
 import theano.tensor as T
-
 class LogisticRegression(object):
+    """Multi-class Logistic Regression Class
+
+    The logistic regression is fully described by a weight matrix :math:`W`
+    and bias vector :math:`b`. Classification is done by projecting data
+    points onto a set of hyperplanes, the distance to which is used to
+    determine a class membership probability.
+    """
 
     def __init__(self, input, n_in, n_out):
+        """ Initialize the parameters of the logistic regression
 
-        #initialize with 0, constructs a matrix of size n_in, n_out
+        :type input: theano.tensor.TensorType
+        :param input: symbolic variable that describes the input of the
+                      architecture (one minibatch)
+
+        :type n_in: int
+        :param n_in: number of input units, the dimension of the space in
+                     which the datapoints lie
+
+        :type n_out: int
+        :param n_out: number of output units, the dimension of the space in
+                      which the labels lie
+
+        """
+        # start-snippet-1
+        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
         self.W = theano.shared(
-            value = numpy.zeros(
+            value=numpy.zeros(
                 (n_in, n_out),
-                dtype = theano.config.floatX
+                dtype=theano.config.floatX
             ),
-            name = 'W',
-            borrow = True #It is a safe practice (and a good idea) to use borrow = True, http://deeplearning.net/software/theano/tutorial/aliasing.html
+            name='W',
+            borrow=True
         )
-
-        #initalize the biases b as a vector of n_out
+        # initialize the baises b as a vector of n_out 0s
         self.b = theano.shared(
-            value = numpy.zeros(
+            value=numpy.zeros(
                 (n_out,),
-                dtype = theano.config.floatX
+                dtype=theano.config.floatX
             ),
-            name = 'b',
-            borrow = True
+            name='b',
+            borrow=True
         )
 
-        #symbolic expression for computing the matrix of class-membership probabilities
-        #W is a matrix where column-k represents the separation for class-k
-        #x is a matrix where row-j represents input training sample-j
-        #b is a vector where element-k represents the free parameter of hyper plain-k
+        # symbolic expression for computing the matrix of class-membership
+        # probabilities
+        # Where:
+        # W is a matrix where column-k represent the separation hyper plain for
+        # class-k
+        # x is a matrix where row-j  represents input training sample-j
+        # b is a vector where element-k represent the free parameter of hyper
+        # plain-k
         self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
 
-        #Symbolic description of how to computer prediction as class whose probablity is maxmial
-        #To get the actual model prediction, we can use the T.argmax operator, which will return the index
-        #at which p_y_given_x is maximal (i.e. the class with maximum probability).
-        self.y_pred = T.argmax(self.p_y_given_x, axis = 1)
+        # symbolic description of how to compute prediction as class whose
+        # probability is maximal
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+        # end-snippet-1
 
+        # parameters of the model
         self.params = [self.W, self.b]
 
     def negative_log_likelihood(self, y):
-        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0], y)])
+        """Return the mean of the negative log-likelihood of the prediction
+        of this model under a given target distribution.
+
+        .. math::
+
+            \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
+            \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|}
+                \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
+            \ell (\theta=\{W,b\}, \mathcal{D})
+
+        :type y: theano.tensor.TensorType
+        :param y: corresponds to a vector that gives for each example the
+                  correct label
+
+        Note: we use the mean instead of the sum so that
+              the learning rate is less dependent on the batch size
+        """
+        # start-snippet-2
+        # y.shape[0] is (symbolically) the number of rows in y, i.e.,
+        # number of examples (call it n) in the minibatch
+        # T.arange(y.shape[0]) is a symbolic vector which will contain
+        # [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
+        # Log-Probabilities (call it LP) with one row per example and
+        # one column per class LP[T.arange(y.shape[0]),y] is a vector
+        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
+        # LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
+        # the mean (across minibatch examples) of the elements in v,
+        # i.e., the mean log-likelihood across the minibatch.
+        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+        # end-snippet-2
+
     def errors(self, y):
-        if y.ndim != self.p_y_given_x:
+        """Return a float representing the number of errors in the minibatch
+        over the total number of examples of the minibatch ; zero one
+        loss over the size of the minibatch
+
+        :type y: theano.tensor.TensorType
+        :param y: corresponds to a vector that gives for each example the
+                  correct label
+        """
+
+        # check if y has same dimension of y_pred
+        if y.ndim != self.y_pred.ndim:
             raise TypeError(
                 'y should have the same shape as self.y_pred',
-                ('y', target.type, 'y_pred', self.y_pred.type)
+                ('y', y.type, 'y_pred', self.y_pred.type)
             )
+        # check if y is of the correct datatype
         if y.dtype.startswith('int'):
-            return T.mean(T.neg(self.y_pred, y))
+            # the T.neq operator returns a vector of 0s and 1s, where 1
+            # represents a mistake in prediction
+            return T.mean(T.neq(self.y_pred, y))
         else:
-            NotImplementedError()
+            raise NotImplementedError()
--- Test.py
+++ Test.py
@@ -13,23 +13,18 @@ import LogisticRegression
 
 from MLP import MLP
 from DataLoader import DataLoader
-from sklearn.cross_validation import KFold
+from sklearn import cross_validation
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
-def load_data():
+def load_data(indices):
     path = 'datasets/ACEInhibitors_processed.csv'
     data_loader = DataLoader(path)
-    indices = [1, 4, 5] #Represents the ith column that must be read. 1: Title, 2: Abstract, and 4: M
     feature_matrix = data_loader.read_column_as_list(indices)[0]
-    print(feature_matrix.shape)
-    sys.exit()
     count_vect = CountVectorizer()
     x_train_counts = count_vect.fit_transform(feature_matrix)
-
     tfidf_transformer = TfidfTransformer()
     x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
-
     return x_train_tfidf
 
 def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000, batch_size=20, n_hidden=500):
@@ -61,37 +56,50 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000, batc
 
    """
 
-    data_sets = load_data()
-    kf = KFold(4, n_folds=2)
-    train_set_x, train_set_y = datasets[0]
-    valid_set_x, valid_set_y = datasets[1]
-    test_set_x, test_set_y = datasets[2]
+    path = 'datasets/ACEInhibitors_processed.csv'
+    data_loader = DataLoader(path)
+   # indices = [1, 4, 5] #Represents the ith column that must be read. 1: Title, 2: Abstract, and 4: M
+    indices = 5
+    target_i = 6
+    data_sets = load_data(indices)
+    target_set = numpy.transpose(numpy.asarray(data_loader.read_column_as_list(target_i)))
 
+    data_sets = numpy.asarray(data_sets.todense())
+    data_sets = data_sets[:2000, :]
+    train_set_stop = data_sets.shape[0]*.5
+    valid_set_stop = train_set_stop+data_sets.shape[0]*.25
+    test_set_stop = valid_set_stop+data_sets.shape[0]*.25
+    train_set_x = data_sets[:train_set_stop, 0:]
+    train_set_y = target_set[:train_set_stop]
+    valid_set_x = data_sets[train_set_stop:valid_set_stop, :]
+    valid_set_y = target_set[train_set_stop:valid_set_stop]
+    test_set_x = data_sets[valid_set_stop:test_set_stop,:]
+    test_set_y = target_set[valid_set_stop:test_set_stop]
     # compute number of minibatches for training, validation and testing
-    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
-    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] / batch_size
-    n_test_batches = test_set_x.get_value(borrow=True).shape[0] / batch_size
-
-    ######################
-    # BUILD ACTUAL MODEL #
-    ######################
-    print('... building the model')
-
+    n_train_batches = int(train_set_x.shape[0] / batch_size)
+    n_valid_batches = int(valid_set_x.shape[0] / batch_size)
+    n_test_batches = int(test_set_x.shape[0] / batch_size)
     # allocate symbolic variables for the data
-    index = T.lscalar()  # index to a [mini]batch
+    index = T.lscalar('index')  # index to a [mini]batch
     x = T.matrix('x')  # the data is presented as rasterized images
     y = T.ivector('y')  # the labels are presented as 1D vector of
                         # [int] labels
-
+    print(type(test_set_y))
+    test_set_x = theano.shared(test_set_x, 'test_set_x')
+    test_set_y = theano.shared(value=test_set_y, name='test_set_y', broadcastable=(False, True))
+    valid_set_x = theano.shared(valid_set_x, 'valid_set_x')
+    valid_set_y = theano.shared(valid_set_y, 'valid_set_y')
+    test_set_y = T.cast(test_set_y, 'int32')
+    train_set_x = theano.shared(train_set_x, 'train_set_x')
+    train_set_y = theano.shared(train_set_y, 'train_set_y')
     rng = numpy.random.RandomState(1234)
-
     # construct the MLP class
     classifier = MLP(
         rng=rng,
         input=x,
-        n_in=28 * 28,
+        n_in=2000,
         n_hidden=n_hidden,
-        n_out=10
+        n_out=1
     )
 
     # start-snippet-4
@@ -104,13 +112,14 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000, batc
         + L2_reg * classifier.L2_sqr
     )
     # end-snippet-4
-
     # compiling a Theano function that computes the mistakes that are made
     # by the model on a minibatch
+
     test_model = theano.function(
         inputs=[index],
         outputs=classifier.errors(y),
         givens={
+
             x: test_set_x[index * batch_size:(index + 1) * batch_size],
             y: test_set_y[index * batch_size:(index + 1) * batch_size]
         }
@@ -173,7 +182,6 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000, batc
                                   # on the validation set; in this case we
                                   # check every epoch
 
-    best_params = None
     best_validation_loss = numpy.inf
     best_iter = 0
     test_score = 0.
@@ -192,7 +200,8 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000, batc
 
             if (iter + 1) % validation_frequency == 0:
                 # compute zero-one loss on validation set
-                validation_losses = [validate_model(i) for i in range(n_valid_batches)]
+                validation_losses = [validate_model(i) for i
+                                     in range(n_valid_batches)]
                 this_validation_loss = numpy.mean(validation_losses)
 
                 print(
@@ -238,7 +247,7 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000, batc
     print >> sys.stderr, ('The code for file ' +
                           os.path.split(__file__)[1] +
                           ' ran for %.2fm' % ((end_time - start_time) / 60.))
-def main():
-    load_data()
-if __name__ == "__main__":
-    main()
+
+
+if __name__ == '__main__':
+    test_mlp()
