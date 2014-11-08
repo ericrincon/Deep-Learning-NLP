27505659c089cf1a375043aecaf0d2f284495039
eric147 <eric_rincon@Erics-MacBook-Pro.local>
2014-11-07 19:55:45 -0600

    Updated preprocessing
    Updated code to look a bit neater and added preprocessing of document
    tokens for bag of words model.

--- DataLoader.py
+++ DataLoader.py
@@ -3,21 +3,59 @@ __author__ = 'eric_rincon'
 from os import listdir
 from pandas import read_csv
 from os.path import isfile
+from array import array
+from sys import exit
+from pandas import DataFrame as df
+import numpy
 import sys
+
+
 class DataLoader(object):
     def __init__(self, path):
         self.dir_path = path
         self.csv_files = self.read_csv_files(self.dir_path)
+    """
+        columns_to_get should be a dictionary that maps the indices to get with their respective names
+    """
+    def get_feature_matrix(self, columns):
+        #feature_matrix: the return value that will contain all the documents with the appropriate
+        #value concatenated to each respective element
+        feature_matrix = []
 
-    def read_column_as_list(self, columns_to_get):
-        matrix = []
+        #True: wil get the feature matrix of the documents
+        #False: will return the column requested
 
+        y_i = columns.pop('y')
+        keys = list(columns.keys())
+
+        #iterate over all csv files provided
         for csv_file in self.csv_files:
-            matrix.append(csv_file.iloc[:,columns_to_get])
-        return matrix
+            n_rows = csv_file.shape[0]
+
+            #create a row that represents one document and contains all features
+            for row in range(n_rows):
+                #document_strings: holds all the elements in the [i,j] portion of the csv file
+                #features: used to concatenate all the elements in the selected columns
+                #feature_vector: represents a document with all the features
+                document_strings = ""
+                features = ""
+                feature_vector = ""
+
+                #iterates over all the keys in the dictionary and
+                for n_key in keys:
+                    document_strings = csv_file.iloc[row,n_key].split()
+                    #concatenate the row name to each respective feature
+                    for string in document_strings:
+                        features += (columns[n_key] + string) + " "
+                    feature_vector+=features
+                    feature_matrix.append(numpy.asarray(feature_vector))
+
+        return [numpy.asarray(feature_matrix), numpy.asarray(csv_file.iloc[:, y_i])]
+
     """Takes in a parameter folder, reads all csv files and then returns a list containing the csv files.
     """
-
+    def get_path(self):
+        return self.dir_path
     def read_csv_files(self, path):
         csv_file_data = []
 
@@ -34,3 +72,4 @@ class DataLoader(object):
                     csv_file_data.append(csv_data)
 
             return csv_file_data
+
--- Test.py
+++ Test.py
@@ -7,6 +7,7 @@ import time
 
 import numpy
 
+
 import theano
 import theano.tensor as T
 import LogisticRegression
@@ -16,16 +17,18 @@ from DataLoader import DataLoader
 from sklearn import cross_validation
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from pandas import DataFrame as df
 
-def load_data(indices):
-    path = 'datasets/ACEInhibitors_processed.csv'
+def get_training_test_m(path, indices):
     data_loader = DataLoader(path)
-    feature_matrix = data_loader.read_column_as_list(indices)[0]
+    data = data_loader.get_feature_matrix(indices)
+    feature_matrix = data[0]
+    y = data[1]
     count_vect = CountVectorizer()
     x_train_counts = count_vect.fit_transform(feature_matrix)
     tfidf_transformer = TfidfTransformer()
     x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
-    return x_train_tfidf
+    return [x_train_tfidf, y]
 
 def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000, batch_size=20, n_hidden=500):
     """
@@ -55,49 +58,48 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000, batc
 
 
    """
+    #names: represents the ith column that must be read. 1: Title, 2: Abstract, 4: MESH, and y: is the column for the target values
 
     path = 'datasets/ACEInhibitors_processed.csv'
-    data_loader = DataLoader(path)
-   # indices = [1, 4, 5] #Represents the ith column that must be read. 1: Title, 2: Abstract, and 4: M
-    indices = 5
-    target_i = 6
-    data_sets = load_data(indices)
-    target_set = numpy.transpose(numpy.asarray(data_loader.read_column_as_list(target_i)))
-
-    data_sets = numpy.asarray(data_sets.todense())
-    data_sets = data_sets[:2000, :]
-    train_set_stop = data_sets.shape[0]*.5
-    valid_set_stop = train_set_stop+data_sets.shape[0]*.25
-    test_set_stop = valid_set_stop+data_sets.shape[0]*.25
-    train_set_x = data_sets[:train_set_stop, 0:]
-    train_set_y = target_set[:train_set_stop]
-    valid_set_x = data_sets[train_set_stop:valid_set_stop, :]
-    valid_set_y = target_set[train_set_stop:valid_set_stop]
-    test_set_x = data_sets[valid_set_stop:test_set_stop,:]
-    test_set_y = target_set[valid_set_stop:test_set_stop]
+    names = {1:'title' ,4:'abstract', 5:'mesh', 'y':6}
+    data = get_training_test_m(path, names)
+    features = data[0]
+    targets = numpy.intc(data[1])
+    data_sets = numpy.asarray(features.todense())
+    data_sets = numpy.transpose(data_sets)
+    data_sets = data_sets[:, :2000]
+    train_set_stop = 2000*.5
+    valid_set_stop = 2000*.25 + train_set_stop
+    test_set_stop = 2000*.25 + valid_set_stop
+    train_set_x = data_sets[:, train_set_stop]
+    train_set_y = targets[:train_set_stop]
+    valid_set_x = data_sets[:, train_set_stop:valid_set_stop]
+    valid_set_y = targets[train_set_stop:valid_set_stop]
+    test_set_x = data_sets[:, valid_set_stop:test_set_stop]
+    test_set_y = targets[valid_set_stop:test_set_stop]
     # compute number of minibatches for training, validation and testing
-    n_train_batches = int(train_set_x.shape[0] / batch_size)
-    n_valid_batches = int(valid_set_x.shape[0] / batch_size)
-    n_test_batches = int(test_set_x.shape[0] / batch_size)
+    n_train_batches = int(2000 / batch_size)
+    n_valid_batches = int(500 / batch_size)
+    n_test_batches = int(500 / batch_size)
     # allocate symbolic variables for the data
     index = T.lscalar('index')  # index to a [mini]batch
     x = T.matrix('x')  # the data is presented as rasterized images
     y = T.ivector('y')  # the labels are presented as 1D vector of
                         # [int] labels
-    print(type(test_set_y))
     test_set_x = theano.shared(test_set_x, 'test_set_x')
-    test_set_y = theano.shared(value=test_set_y, name='test_set_y', broadcastable=(False, True))
+    test_set_y = theano.shared(test_set_y, 'test_set_y')
     valid_set_x = theano.shared(valid_set_x, 'valid_set_x')
     valid_set_y = theano.shared(valid_set_y, 'valid_set_y')
-    test_set_y = T.cast(test_set_y, 'int32')
     train_set_x = theano.shared(train_set_x, 'train_set_x')
     train_set_y = theano.shared(train_set_y, 'train_set_y')
+
     rng = numpy.random.RandomState(1234)
     # construct the MLP class
+
     classifier = MLP(
         rng=rng,
         input=x,
-        n_in=2000,
+        n_in=features.shape[0],
         n_hidden=n_hidden,
         n_out=1
     )
@@ -119,7 +121,6 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000, batc
         inputs=[index],
         outputs=classifier.errors(y),
         givens={
-
             x: test_set_x[index * batch_size:(index + 1) * batch_size],
             y: test_set_y[index * batch_size:(index + 1) * batch_size]
         }
